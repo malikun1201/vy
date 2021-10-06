3811afaea7ca3f88d46f7f0282184db81a12f8e4
Matthew K <wabasi333@gmail.com>
2021-10-06 10:42:38 -0500

    Cleaning up some Files

---
+++ src/test/java/com/vytrack/tests/matthew_User_Story4/UserStory4_AC_1.java
@@ -0,0 +1,54 @@
+package com.vytrack.tests.matthew_User_Story4;
+
+import com.vytrack.pages.VyTrackLoginPage;
+import com.vytrack.utility.TestBase;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class UserStory4_AC_1 extends TestBase {
+
+
+/*
+    @Test
+    public void CreateContactInfoTest(){
+
+        Map<String, String> driverMap = new HashMap<>();
+        driverMap.put("user13", "UserUser123");
+        driverMap.put("user14", "UserUser123");
+        driverMap.put("user15", "UserUser123");
+        Map<String, String> storeManagerMap = new HashMap<>();
+        storeManagerMap.put("storemanager59", "UserUser123");
+        storeManagerMap.put("storemanager60", "UserUser123");
+        Map<String, String> salesManagerMap = new HashMap<>();
+        salesManagerMap.put("salesmanager113", "UserUser123");
+        salesManagerMap.put("salesmanager114", "UserUser123");
+        salesManagerMap.put("salesmanager115", "UserUser123");
+
+        VyTrackLoginPage loginPage = PageFactory.initElements(driver,VyTrackLoginPage.class);
+
+        loginPage.setUsernameField("user13");
+        loginPage.setPasswordField("UserUser123");
+        loginPage.clickSubmitButton();
+
+
+    }
+*/
+
+}
+
+
+/*
+4. Story: As a user, I should be able to create contact information.
+AC #1: users are able to create contacts.
+Given user is on the homePage
+When user select “Contacts” under Customers module
+And user click “Create Contact” button
+When user fill out general information
+And click “Save and Close” button
+Then verify “contact saved” confirm message
+ */
