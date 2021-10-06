777e4d01e0b3b1aea39a4121fbbf683c9c91dd9b
YurovaAlina <kulunbiegova@gmail.com>
2021-10-06 11:50:15 -0500

    started userstory

---
+++ src/test/java/com/vytrack/tests/Alina_User_Story9/UserStory9.java
@@ -0,0 +1,18 @@
+package com.vytrack.tests.Alina_User_Story9;
+
+public class UserStory9 {
+    /*
+    As a user, I should be create Vehicle Contract.
+AC #1: managers are able to Create Vehicle Contract.
+Given store/sales manager is on the homePage
+When user select “Vehicle Contract” under Fleet module
+And user click “Create Vehicle Contract” button
+When user fill out general information and click “Save and Close” button
+Then verify “Entity saved” confirm message
+
+AC #2: driver should not to create Vehicle Contract.
+Given driver is on the homePage
+When user select “Vehicle Contract” under Fleet module
+Then verify “You do not have permission to perform this action.” message
+     */
+}
