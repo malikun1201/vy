584755f22cdeb3793445e800f288dc38ad8f06b0
ChristinaDolganiuc <74195190+ChristinaDT@users.noreply.github.com>
2021-10-08 15:04:49 -0500

    VehicleOdometerPage updated with new locators

--- src/test/java/com/vytrack/pages/VehicleOdometerPage.java
+++ src/test/java/com/vytrack/pages/VehicleOdometerPage.java
@@ -2,6 +2,9 @@ package com.vytrack.pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import static org.openqa.selenium.By.xpath;
 
 public class VehicleOdometerPage {
 
@@ -11,28 +14,35 @@ public class VehicleOdometerPage {
     @FindBy(linkText = "Fleet")
     public WebElement fleetModuleDriver;
 
-    @FindBy(partialLinkText = "VehicleOdometer")
+    @FindBy(xpath=" //span[contains(text(),'Vehicle Odometer')]")
     public WebElement vehicleOdometer;
 
     @FindBy(xpath = "//h1[contains(text(),'Vehicles Odometers')]")
     public WebElement vehicleOdometerMsg;
 
-    @FindBy(xpath = "//a[contains(text(),'Create Vehicle Odometer')]")
+    @FindBy(linkText = "Create Vehicle Odometer")
     public WebElement createOdometerBtn;
 
-    @FindBy(id = "custom_entity_type_OdometerValue-uid-615bcf79190e2")
+    @FindBy(css = "[name='custom_entity_type[OdometerValue]']")
     public WebElement odometerValueBox;
 
-    @FindBy(id = "date_selector_custom_entity_type_Date-uid-615bcf79191ec")
+    @FindBy(css = "[name='date_selector_custom_entity_type_Date-uid-61609aeb1e8dd']")
     public WebElement dateBox;
 
-    @FindBy(id = "custom_entity_type_Driver-uid-615bcf79192fa")
+    @FindBy(css = "[name='custom_entity_type[Driver]']")
     public WebElement driverBox;
 
-    @FindBy(id = "s2id_custom_entity_type_Units-uid-615bcf7919466")
-    public WebElement unitBox; //javaScriptExecutor-selectByName - miles/kg
+    @FindBy(className = "select2-chosen")
+    public WebElement unitBox; //javaScriptExecutor-selectByName - miles/km
 
     @FindBy(xpath = "//button[contains(text(),'Save and Close')]")
     public WebElement saveAndCloseBtn;
+
+    @FindBy(className = "message")
+    public WebElement entityMsg;
+
+    public VehicleOdometerPage(){
+        PageFactory.initElements(com.vytrack.utility.Driver.getDriver(), this);
+    }
 
 }
